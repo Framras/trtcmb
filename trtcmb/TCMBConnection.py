d6574130551389c87696b6baecbbabb7e533e1af
Tufan Kaynak KTA <tufan.kaynak@framras.com>
2024-12-11 00:31:18 +0300

    Total Requests to TCMB dropped to 2

--- trtcmb/TCMBConnection.py
+++ trtcmb/TCMBConnection.py
@@ -38,12 +38,12 @@ class TCMBConnection:
         tcmb_start_date = datetime.date.today()
         if self.start_date is not None and self.start_date > datetime.date(1950, 1, 2):
             tcmb_start_date = self.start_date
-        for currency in currency_list:
-            tcmb_exchange_rates = self.get_exchange_rates(currency=currency.get("currency_name"),
-                                                          from_date=tcmb_start_date,
-                                                          to_date=datetime.date.today())
-            for tcmb_exchange_rate_data in tcmb_exchange_rates:
-                TCMBCurrencyExchange.commit_single_currency_exchange_rate(tcmb_exchange_rate_data, self.enable_update)
+        # for currency in currency_list:
+        tcmb_exchange_rates = self.get_exchange_rates(currency_list=currency_list,
+                                                      from_date=tcmb_start_date,
+                                                      to_date=datetime.date.today())
+        for tcmb_exchange_rate_data in tcmb_exchange_rates:
+            TCMBCurrencyExchange.commit_single_currency_exchange_rate(tcmb_exchange_rate_data, self.enable_update)
 
         return datetime.datetime.today().date()
 
@@ -85,42 +85,50 @@ class TCMBConnection:
                 response_dict["items"][0][currency_response] = new_dict["items"][0][currency_response]
         return response_dict
 
-    def get_exchange_rates(self, currency: str, from_date: datetime.date, to_date: datetime.date):
+    def get_exchange_rates(self, currency_list: list, from_date: datetime.date, to_date: datetime.date):
         # dummy assignment
         currency_series_as_list = list()
         # if purpose == "for_buying":
-        currency_series_as_list.append(self.inner_separator.join(
-            ["TP", "DK", currency, TCMBCurrencyExchange.buying_code]))
-        # elif purpose == "for_selling":
-        currency_series_as_list.append(self.inner_separator.join(
-            ["TP", "DK", currency, TCMBCurrencyExchange.selling_code]))
+        for currency in currency_list:
+            currency_series_as_list.append(self.inner_separator.join(
+                ["TP", "DK", currency.get("currency_name"), TCMBCurrencyExchange.buying_code]))
+            # elif purpose == "for_selling":
+            currency_series_as_list.append(self.inner_separator.join(
+                ["TP", "DK", currency.get("currency_name"), TCMBCurrencyExchange.selling_code]))
         # Exchange, rates, Daily, (Converted, to, TRY)
         response_dict = self.connect(datagroup_code=self.datagroup_code, series_list=currency_series_as_list,
                                      for_start_date=from_date, for_end_date=to_date)
         if response_dict.get("totalCount") >= 1:
             return_list = list()
             currency_series_data = response_dict.pop("items")
+            reference_dict = dict()
             for currency_tuple in currency_series_data:
-                for tcmb_series in currency_series_as_list:
-                    currency_response = tcmb_series.replace(self.inner_separator,
-                                                            TCMBCurrencyExchange.response_separator)
-                    if currency_tuple.get(currency_response) is None:
-                        exchange_rate_date = datetime.datetime.strptime(currency_tuple.get("Tarih"),
+                currency_tuple.pop(TCMBCurrencyExchange.tcmb_strip_key)
+                reference_date = currency_tuple.pop("Tarih")
+                for tuple_key in list(currency_tuple):
+                    reference_dict[reference_date + tuple_key] = currency_tuple.get(tuple_key)
+                    # response_currency = tuple_key.replace(TCMBCurrencyExchange.response_separator,
+                    #                                         self.inner_separator)
+                    if currency_tuple.get(tuple_key) is None:
+                        exchange_rate_date = datetime.datetime.strptime(reference_date,
                                                                         TCMBCurrencyExchange.tcmb_date_format).date() - \
                                              self.a_day
-                        tcmb_series_split = str(tcmb_series).split(".")
+                        # tcmb_series_split = str(tcmb_series).split(".")
+                        tcmb_series_split = str(tuple_key).split("_")
                         # TODO: Check later if purpose is blank or not
                         purpose = ""
                         if tcmb_series_split[3] == TCMBCurrencyExchange.buying_code:
                             purpose = "for_buying"
                         if tcmb_series_split[3] == TCMBCurrencyExchange.selling_code:
                             purpose = "for_selling"
-                        new_dict = self.get_single_exchange_rate(currency, exchange_rate_date, purpose=purpose)
-                        return_list.append({currency_response: new_dict["items"][0][currency_response],
-                                            TCMBCurrencyExchange.tcmb_date_key: currency_tuple.get(
-                                                TCMBCurrencyExchange.tcmb_date_key)})
-                    else:
-                        return_list.append({currency_response: currency_tuple.get(currency_response),
-                                            TCMBCurrencyExchange.tcmb_date_key: currency_tuple.get(
-                                                TCMBCurrencyExchange.tcmb_date_key)})
+                        if reference_dict.get(
+                            datetime.datetime.strftime(exchange_rate_date, '%d-%m-%Y') + tuple_key) is None:
+                            new_dict = self.get_single_exchange_rate(tcmb_series_split[2], exchange_rate_date,
+                                                                     purpose=purpose)
+                            currency_tuple[tuple_key]=new_dict["items"][0][tuple_key]
+                        else:
+                            currency_tuple[tuple_key]=reference_dict.get(
+                            datetime.datetime.strftime(exchange_rate_date, '%d-%m-%Y') + tuple_key)
+                currency_tuple[TCMBCurrencyExchange.tcmb_date_key]=reference_date
+                return_list.append(currency_tuple)
             return return_list
